77afe5a5034ca029564537211becda020a5d3ab3
sakominov <theslowryz@gmail.com>
2023-08-09 17:17:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use('/auth', authRouter)
+
+const start = () => {
+    try {
+        app.listen(PORT, async () => {
+            await mongoose.connect(`url`)
+            console.log(`server started on port ${PORT}`)
+        })
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+start()
